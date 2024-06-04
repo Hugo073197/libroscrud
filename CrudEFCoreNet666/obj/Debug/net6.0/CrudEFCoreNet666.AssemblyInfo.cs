1958a490aa19775c9fe49af8ccd6a68104cd2cd8
Hugo <armando_073197@hotmail.com>
2024-06-04 07:41:02 -0600

    movimos el archivo practica.cs

--- CrudEFCoreNet666/obj/Debug/net6.0/CrudEFCoreNet666.AssemblyInfo.cs
+++ CrudEFCoreNet666/obj/Debug/net6.0/CrudEFCoreNet666.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudEFCoreNet666")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d64b2e030f56fd456c3426820cd4e8703a99d948")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudEFCoreNet666")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudEFCoreNet666")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
